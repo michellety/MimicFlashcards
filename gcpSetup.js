c45a885fb7f2d9c800754a40e2d547ee93df6d47
michelle <michelletyson29@gmail.com>
2019-06-04 20:33:35 -0700

    changed file key name

--- gcpSetup.js
+++ gcpSetup.js
@@ -1,3 +1,5 @@
-var fs = require("fs");
-fs.writeFile(process.env.GOOGLE_APPLICATION_CREDENTIALS, process.env.GCP_CRED, (err) => {});
+// var fs = require("fs");
+// fs.writeFile(process.env.GOOGLE_APPLICATION_CREDENTIALS, process.env.GCP_CRED, (err) => {});
 // fs.writeFile('secrete.json', process.env.GCP_CRED, (err) => {});
+var fs = require("fs");
+fs.writeFile(process.env.GCP_KEY_FILE, process.env.GCP_CRED, (err) => {});
